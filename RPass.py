1e269ef89396a05d31c1c98a7fcecbb8bdf71f37
Zeni <satanasviene2@gmail.com>
2026-01-12 20:16:59 -0500

    RPass.py
    v0.2
    - Added copy-to-clipboard function.
    - Updated menu descriptions.
    - Password now returns.
    - Solved some minor bugs & typos.

--- RPass.py
+++ RPass.py
@@ -1,48 +1,104 @@
 import random as r
+import pyperclip as pc
 #By Zenitharss
-usable_chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789@#"
+def pcopy(password):
+    while True:
+        try:
+            print("Do you want to copy your password to the clipboard?")
+            print("1 - Yes.")
+            print("2 - No.")
+            copy_to_clipboard = int(input("Select an option (1 or 2): "))
+            match copy_to_clipboard:
+                case 1:
+                    pc.copy(password)
+                    print("Copied to the clipboard")
+                    break
+                case 2:
+                    print("Returning...")
+                    break
+                case _:
+                    print("Error.")
+        except ValueError: print("Error.")
+    
+def rPass(usable_chars):
+        while True:
+            try:
+                print("PASSWORD LENGTH OPTIONS:")
+                print("1. 8 characters")
+                print("2. 16 characters")
+                print("3. Custom length")
+                print("4. Back to main menu")
+                condicion = int(input("Select option (1-4): "))
+                match condicion:
+                    case 1:
+                        print("-8 CHARACTER PASSWORD RANDOMIZER...-")
+                        password = ''.join(r.choice(usable_chars)for _ in range(8)) 
+                        print(f"Your password is: {password}")
+                        pcopy(password)
+                        return password
+                    case 2:
+                        print("-16 CHARACTER PASSWORD RANDOMIZER...-")
+                        password = ''.join(r.choice(usable_chars)for _ in range(16))
+                        print(f"Your password is: {password}")
+                        pcopy(password)
+                        return password
+                    case 3:
+                        print("-CUSTOM LENGTH CHARACTER PASSWORD RANDOMIZER...-")
+                        rang = int(input("Introduce the number of characters, or 0 to go back: "))
+                        if rang ==0:
+                            print("Returning...")
+                            break
+                        elif 0 < rang <= 100:
+                            password =''.join(r.choice(usable_chars) for _ in range(rang))
+                            print(f"Your password is: {password}")
+                            pcopy(password)
+                            return password
+                        else: print("The number can't be a number over 100 or below 0.")
+                    case 4: 
+                        print("RETURNING...")
+                        break
+                    case _:
+                        print("Error")
+            except ValueError:
+                print("Error.")
 def ZrPass():
+    usable_chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789"
     while True:
         try:
             print("- zPASSWORD RANDOMIZER -")
-            opcion = int(input("1-Randomize 0- Exit."))
+            print("1 - Randomize.")
+            print("2 - Exit.")
+            opcion = int(input("Select an option (1 or 2): "))
             match opcion:
                 case 1:
                     while True: 
                         try:
-                            print("RANDOMIZER...")
-                            condicion = int(input("8, 16 or custom chars password? (1, 2 or 3. 0 to exit.): "))
-                            match condicion:
+                            print("-RANDOMIZER...-")
+                            print("Specify the characters the password could contain?")
+                            print("1 - Yes.")
+                            print("2 - No.")
+                            print("0 - Exit.")
+                            usable_confirmation = int(input("Select an option (1, 2 or 0): "))
+                            match usable_confirmation:
                                 case 1:
-                                    print("8 CHARACTER PASSWORD RANDOMIZER...")
-                                    password = ''.join(r.choice(usable_chars)for _ in range(8)) 
-                                    print(f"Your password is: {password}")
-                                    break
+                                    print("")
+                                    usable_chars = input("Write the usable characters: ")
+                                    password = rPass(usable_chars)
+                                    return password
                                 case 2:
-                                    print("16 CHARACTER PASSWORD RANDOMIZER...")
-                                    password = ''.join(r.choice(usable_chars)for _ in range(16))
-                                    print(f"Your password is: {password}")
+                                    print("-NORMAL RANDOMIZER-")
+                                    password = rPass(usable_chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789")
+                                    return password
+                                case 0:
+                                    print("Exiting...")
                                     break
-                                case 3:
-                                    rang = int(input("INTRODUCE THE NUMBER OF CHARACTERS: "))
-                                    print("CUSTOM CHARACTER PASSWORD")
-                                    password =''.join(r.choice(usable_chars) for i in range(rang))
-                                    print(f"Your password is: {password}")
-                                    match rang:
-                                        case 0:
-                                            print("Error")
-                                case 0: 
-                                    print("EXITING...")
-                                    break
-                                case _:
-                                    print("Error")
+                                case _: print("Error.")
                         except ValueError: print("Error")
-                case 0:
+                case 2:
                     print("Exiting...")
-                    break
+                    return None
                 case _:
                     print("Error")
         except ValueError: print("Error")
 password = ZrPass()
-
 print(password)
